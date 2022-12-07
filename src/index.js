9d847ef9a6ab0859e3fd5176504420177eeaaf93
Misha Dzhemula <medvedmisha23@gmail.com>
2022-12-07 17:23:49 +0200

    Improvement

--- src/index.js
+++ src/index.js
@@ -11,51 +11,58 @@ function sendAnErr(res, err) {
   res.end(String(err));
 }
 
+function handleIndex(req, res) {
+  const index = fs.createReadStream('./src/public/index.html');
+
+  pipeline(index, res, err => {
+    if (err) {
+      sendAnErr(res, err);
+    }
+  });
+}
+
+function handleSend(req, res) {
+  const form = formidable();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      sendAnErr(res, err);
+
+      return;
+    }
+
+    const fileStream = fs.createReadStream(files.uploadedFile.filepath);
+    let compressionBox;
+    const compressionType = fields.compressionType;
+
+    res.setHeader(
+      'Content-Disposition',
+      `attachment: filename=${files.uploadedFile.originalFilename}`
+    );
+
+    switch (compressionType) {
+      case 'gzip':
+        compressionBox = zlib.createGzip();
+        break;
+      case 'br':
+        compressionBox = zlib.createBrotliCompress();
+    }
+
+    pipeline(fileStream, compressionBox, res, error => {
+      if (error) {
+        sendAnErr(res, error);
+      }
+    });
+  });
+}
+
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    const index = fs.createReadStream('./src/public/index.html');
-
-    pipeline(index, res, err => {
-      if (err) {
-        sendAnErr(res, err);
-      }
-    });
+    handleIndex(req, res);
   }
 
-  if (req.url === '/sent' && req.method === 'POST') {
-    const form = formidable();
-
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        sendAnErr(res, err);
-
-        return;
-      }
-
-      const fileStream = fs.createReadStream(files.uploadedFile.filepath);
-      let compressionBox;
-      const compressionType = fields.compressionType;
-
-      res.setHeader(
-        'Content-Disposition',
-        `attachment: filename=${files.uploadedFile.originalFilename}`
-      );
-      // res.setHeader('Content-Encoding', `${compressionType}`);
-
-      switch (compressionType) {
-        case 'gzip':
-          compressionBox = zlib.createGzip();
-          break;
-        case 'br':
-          compressionBox = zlib.createBrotliCompress();
-      }
-
-      pipeline(fileStream, compressionBox, res, error => {
-        if (err) {
-          sendAnErr(res, error);
-        }
-      });
-    });
+  if (req.url === '/send' && req.method === 'POST') {
+    handleSend(req, res);
   }
 });
 
