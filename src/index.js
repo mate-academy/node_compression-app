09ee131962ff9f97fc05cc5a126a72c9941235d7
Nazar Brunarskyi <3nnazar836@gmail.com>
2023-02-09 18:27:50 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,92 @@
 'use strict';
+
+const http = require('http');
+const fs = require('fs');
+const formidable = require('formidable');
+const zlib = require('zlib');
+const { pipeline } = require('stream');
+
+const server = new http.Server();
+
+server.on('request', (req, res) => {
+  const normalizedUrl = new URL(req.url, `http://${req.headers.host}`).pathname;
+
+  if (normalizedUrl === '/' || normalizedUrl === '/index.html') {
+    res.setHeader('Content-Type', 'text/html');
+
+    const file = fs.createReadStream('public/index.html');
+
+    file.pipe(res);
+
+    res.on('close', () => {
+      file.destroy();
+    });
+
+    return;
+  }
+
+  if (normalizedUrl === '/index.css') {
+    res.setHeader('Content-Type', 'text/css');
+
+    const file = fs.createReadStream('public/index.css');
+
+    file.pipe(res);
+
+    res.on('close', () => {
+      file.destroy();
+    });
+
+    return;
+  }
+
+  if (normalizedUrl === '/compressFile') {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        res.statusCode = 400;
+        res.end('error has sprang!');
+      }
+
+      const file = files.file.filepath;
+      const compressWith = fields.compression;
+      const filename = files.file.originalFilename;
+      const fileStream = fs.createReadStream(file);
+      const contentType = files.file.mimetype;
+
+      let chosenCompression;
+
+      switch (compressWith) {
+        case 'Brotli':
+          chosenCompression = zlib.createBrotliCompress();
+          break;
+
+        case 'Gzip':
+          chosenCompression = zlib.createGzip();
+          break;
+
+        default:
+          break;
+      }
+
+      res.setHeader('Content-Type', `${contentType}`);
+
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename="${filename}"`
+      );
+
+      pipeline(fileStream, chosenCompression, res, () => { });
+
+      res.on('close', () => {
+        fileStream.destroy();
+      });
+    });
+
+    return;
+  }
+
+  res.end('invalid URL');
+});
+
+server.listen(3000);
