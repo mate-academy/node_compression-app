d30e3b9b9bac2d3a5b4bd543292691a6af88a344
Misha Ivchenko <ivchenkomykhail@meta.ua>
2023-05-10 11:46:02 +0300

    The solution was refactored

--- server/app.js
+++ server/app.js
@@ -2,106 +2,93 @@
 
 const http = require('http');
 const fs = require('fs');
-const { pipeline } = require('stream');
 const zlib = require('zlib');
-const { StringDecoder } = require('node:string_decoder');
-const { multiparty } = require('multiparty');
+const formidable = require('formidable');
+const { pipeline } = require('stream');
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Credentials', 'true');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+const getZlibInstance = (compressionType) => {
+  try {
+    switch (compressionType) {
+      case 'gzip':
+        return zlib.createGzip();
+      case 'gunzip':
+        return zlib.createGunzip();
+      case 'deflate':
+        return zlib.createDeflate();
+      default:
+        return zlib.createGzip();
+    };
+  } catch (error) {
+    return error;
+  }
+};
 
-  res.setHeader('Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With,'
-      + 'Content-Type, Access-Control-Request-Method,'
-      + 'Access-Control-Request-Headers');
+const handleIndex = (req, res) => {
+  res.setHeader('Content-type', 'text/html');
+
+  const fileStream = fs.createReadStream('./public/index.html');
+
+  fileStream.pipe(res);
+
+  fileStream.on('end', () => {
+    // eslint-disable-next-line no-console
+    console.log('Completed');
+  });
+
+  fileStream.on('error', () => {
+    res.statusCode = 500;
+    res.end('Server error');
+  });
+};
+
+const handleUpload = (req, res) => {
   res.setHeader('Content-Disposition', 'attachment');
 
-  console.log('Request received');
-  // console.log(req.file.originalname);
-  // console.log(req);
-  // console.log(req.read());
-
-  // let body = '';
-
-  // const gzip = zlib.createGzip();
-
-  let body = '';
-  let fileData = null;
-  let filePath = '';
-  let compressionType = '';
-
-  req.on('data', (chunk) => {
-    body += chunk;
-
-    if (body.includes('\r\n--')) {
-      const parts = body.split('\r\n--');
-
-      for (const part of parts) {
-        const [header, data] = part.split('\r\n\r\n');
-
-        if (header.includes('filename=')) {
-          const [, name] = header.split('filename=');
-
-          filePath = name.replace(/"/g, '');
-
-          fileData = data.substring(0, data.lastIndexOf('\r\n'));
-        } else if (header.includes('name="data"')) {
-          const index = part.indexOf('name="data"') + 11;
-
-          compressionType = JSON.parse(part.slice(index)).type;
-        }
-      }
-    }
+  const form = formidable({
+    multiples: true, uploadDir: './temp',
   });
 
-  req.on('end', () => {
-    const lastIndex = filePath.lastIndexOf('Content-Type');
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+      res.end(String(err));
 
-    const fileName = './public/' + filePath.slice(0, lastIndex - 2);
+      return;
+    }
 
-    fs.writeFile(fileName, fileData, (err) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end('Server Error');
-      } else {
-        let zip = zlib.createGzip();
+    const zip = getZlibInstance(fields.compressionType);
 
-        switch (compressionType) {
-          case 'gzip':
-            zip = zlib.createGzip();
-            break;
-          case 'gunzip':
-            zip = zlib.createGunzip();
-            break;
-          case 'inflate':
-            zip = zlib.createInflate();
-            break;
-        };
+    const file = files.file;
 
-        const fileStream = fs.createReadStream(fileName);
-        const writeStream = fs.createWriteStream(fileName + '.gz');
+    const fileStream = fs.createReadStream(file.filepath);
 
-        fileStream.pipe(zip).pipe(writeStream);
+    pipeline(fileStream, zip, res, (error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      res.statusCode = 500;
+      res.end('Server error');
+    });
 
-        writeStream.on('finish', () => {
-          res.setHeader('Content-Type', 'application/octet-stream');
-
-          res.setHeader('Content-Disposition', 'attachment; filename='
-            + fileName + '.gz');
-          fs.createReadStream(fileName + '.gz').pipe(res);
-          res.setHeader('Content-Type', 'application/json');
-          res.statusCode = 200;
-        });
-      }
+    res.on('error', (error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      res.statusCode = 500;
+      res.end('Server error');
     });
   });
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/upload' && req.method === 'POST') {
+    handleUpload(req, res);
+  } else {
+    handleIndex(req, res);
+  }
 });
 
 server.on('error', () => {});
 
-server.listen(5001, () => {
+server.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Server started! 🚀');
 });
