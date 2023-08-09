dd3757237e4f0c64d948edc50d645b8a64c28542
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-09 11:50:09 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -6,83 +6,96 @@ const path = require('path');
 const fs = require('fs');
 const { pipeline } = require('stream');
 
+const PATHS = {
+  index: 'GET /',
+  upload: 'POST /upload',
+};
+
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  if (req.url === '/' && req.method === 'GET') {
-    const filePath = path.join(__dirname, 'index.html');
+  switch (req.method + ' ' + req.url) {
+    case PATHS.index:
+      const filePath = path.join(__dirname, 'index.html');
 
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHeader(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal server error');
-      } else {
-        res.writeHeader(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else if (req.url === '/upload' && req.method === 'POST') {
-    const chunks = [];
-
-    req.on('data', (chunk) => {
-      chunks.push(chunk);
-    });
-
-    req.on('end', () => {
-      const data = Buffer.concat(chunks).toString();
-
-      const compressionType = data.slice(data.indexOf('"compressions"') + 15)
-        .trim()[0];
-
-      let compressionStream;
-      let compressionName;
-
-      switch (compressionType) {
-        case 'g':
-          compressionStream = zlib.createGzip();
-          compressionName = 'gzip';
-          break;
-        case 'b':
-          compressionStream = zlib.createBrotliCompress();
-          compressionName = 'brotli';
-          break;
-        case 'd':
-          compressionStream = zlib.createDeflate();
-          compressionName = 'deflate';
-          break;
-      }
-
-      const fileName = /filename="(.+)"/.exec(data)[1];
-      const contetType = /Content-Type: (.+)/.exec(data)[1];
-
-      res.writeHeader(200, {
-        // eslint-disable-next-line max-len
-        'Content-Disposition': `attachment; filename=${fileName}.{${compressionName}}}`,
-        'Content-Type': contetType,
-      });
-
-      const dataPart = data.slice(data.indexOf('\r\n\r\n') + 4);
-      const dataClean = dataPart.slice(
-        0, dataPart.indexOf('------')
-      ).trim();
-
-      pipeline(dataClean, compressionStream, res, (err) => {
+      fs.readFile(filePath, (err, data) => {
         if (err) {
           res.writeHeader(500, { 'Content-Type': 'text/plain' });
           res.end('Internal server error');
+        } else {
+          res.writeHeader(200, { 'Content-Type': 'text/html' });
+          res.end(data);
         }
       });
+      break;
 
-      res.on('close', () => {
-        compressionStream.destroy();
+    case PATHS.upload:
+      const chunks = [];
+
+      req.on('data', (chunk) => {
+        chunks.push(chunk);
       });
-    });
-  } else {
-    res.writeHeader(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
+
+      req.on('end', () => {
+        const data = Buffer.concat(chunks).toString();
+
+        const compressionType = data.slice(data.indexOf('"compressions"') + 15)
+          .trim()[0];
+
+        let compressionStream;
+        let compressionName;
+
+        switch (compressionType) {
+          case 'g':
+            compressionStream = zlib.createGzip();
+            compressionName = 'gzip';
+            break;
+          case 'b':
+            compressionStream = zlib.createBrotliCompress();
+            compressionName = 'brotli';
+            break;
+          case 'd':
+            compressionStream = zlib.createDeflate();
+            compressionName = 'deflate';
+            break;
+        }
+
+        const fileName = /filename="(.+)"/.exec(data)[1];
+        const contetType = /Content-Type: (.+)/.exec(data)[1];
+
+        res.writeHeader(200, {
+          // eslint-disable-next-line max-len
+          'Content-Disposition': `attachment; filename=${fileName}.${compressionName}`,
+          'Content-Type': contetType,
+        });
+
+        const dataPart = data.slice(data.indexOf('\r\n\r\n') + 4);
+        const dataClean = dataPart.slice(
+          0, dataPart.indexOf('------')
+        ).trim();
+        // ------ is the beginning of the next part of the request
+        // so we need to cut it off
+
+        pipeline(dataClean, compressionStream, res, (err) => {
+          if (err) {
+            res.writeHeader(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal server error');
+          }
+        });
+
+        res.on('close', () => {
+          compressionStream.destroy();
+        });
+      });
+      break;
+
+    default:
+      res.writeHeader(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found');
   }
 });
 
 server.on('error', () => {});
 
-server.listen(3000);
+// eslint-disable-next-line no-console
+server.listen(3000, () => console.log('Server started'));
