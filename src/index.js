96a46218e2935410af29fa285add2a3f774e339e
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-09-21 22:34:29 +0300

    changes after code review

--- src/index.js
+++ src/index.js
@@ -3,42 +3,100 @@
 
 const http = require('http');
 const fs = require('fs');
-const zlip = require('zlib');
+const zlib = require('zlib');
 const path = require('path');
-const { pipeline } = require('stream');
+const dotenv = require('dotenv');
+const { pipeline, Transform } = require('stream');
 
-const PORT = 8080;
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  const normalized = new URL(req.url, `http://${req.headers.host}`);
-  const fileName = normalized.pathname.slice(1) || 'index.html';
-  const filePath = path.resolve('public', fileName);
+  if (req.method === 'POST') {
+    const normalized = new URL(req.url, `http://${req.headers.host}`);
+    const fileName = normalized.pathname.slice(1) || 'index.html';
+    const filePath = path.resolve('public', fileName);
+    const compressionType = normalized.searchParams.get('compressionType');
 
-  if (!fs.existsSync(filePath)) {
-    res.statusCode = 404;
-    res.end('File not found');
+    if (!fs.existsSync(filePath)) {
+      res.setHeader('Content-type', 'text/plain');
+      res.statusCode = 404;
+      res.end('File not found');
+
+      return;
+    }
+
+    const compress = compressionType === 'brotli'
+      ? zlib.createBrotliCompress()
+      : zlib.createGzip();
+
+    const typeOfCompress = compressionType === 'brotli'
+      ? 'br'
+      : 'gzip';
+
+    const readStream = fs.createReadStream(filePath);
+    const prependInfoStream = new Transform({
+      transform(chunk, _, callback) {
+        const info = `Compressed by: ${compressionType}\n\n`;
+
+        this.push(info);
+        this.push(chunk);
+        callback();
+      },
+    });
+
+    res.setHeader('Content-Encoding', typeOfCompress);
+    res.setHeader('Content-type', 'text/plain');
+
+    pipeline(readStream, prependInfoStream, compress, res, (error) => {
+      if (error) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('Something went wrong');
+      }
+    });
+
+    res.on('close', () => {
+      readStream.destroy();
+      compress.destroy();
+    });
 
     return;
   }
 
-  const fileStream = fs.createReadStream(filePath);
-  const brotli = zlip.createBrotliCompress();
+  res.setHeader('Content-Type', 'text/html');
 
-  res.setHeader('Content-Encoding', 'br');
-  res.setHeader('Content-type', 'text/plain');
+  res.end(`
+    <h1>Hello</h1>
+    <form method="POST">
+      <label>
+        Choose a type of compression
+        <select name="compressionType">
+          <option value="brotli">Brotli</option>
+          <option value="gzip">Gzip</option>
+        </select>
+      </label>
 
-  pipeline(fileStream, brotli, res, (error) => {
-    if (error) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Something went wrong');
-    }
-  });
+      <button type="submit">
+        Submit
+      </button>
+    </form>
 
-  res.on('close', () => {
-    fileStream.destroy();
-    brotli.destroy();
-  });
+    <script>
+      const select = document.querySelector('select');
+      const newURL = window.location.pathname + '?' +
+        'compressionType=' + select.value.toString();
+      history.pushState(null, null, newURL);
+
+      select.addEventListener('change', (e) => {
+        const searchParams = new URLSearchParams();
+        searchParams.set('compressionType', e.target.value);
+        const newURL = window.location.pathname + '?' + searchParams.toString()
+        history.pushState(null, null, newURL);
+      })
+    </script>
+  `);
 });
 
 server.on('error', (error) => {
