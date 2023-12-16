cce5b416707e18c660627d98507a53ea8c85def7
Dmytro <dmytro2105@gmail.com>
2023-12-16 14:10:02 +0200

    app js

---
+++ src/app.js
@@ -0,0 +1,145 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable curly */
+/* eslint-disable no-shadow */
+/* eslint-disable max-len */
+/* eslint-disable no-useless-return */
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable no-console */
+'use strict';
+
+const PORT = process.env.PORT || 5001;
+
+const http = require('http');
+const zlib = require('zlib');
+const formidable = require('formidable');
+const fs = require('fs');
+const { pipeline } = require('stream');
+// const path = require('path');
+
+const server = new http.Server();
+
+server.on('request', (req, res) => {
+  // const myURL = new URL(req.url, `http://localhost:${PORT}`);
+  // console.log(normalizedURL);
+
+  if (req.method === 'POST' && req.url === '/upload') {
+    const form = new formidable.IncomingForm();
+    // console.log(form);
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        res.statusCode = 400;
+        res.end(JSON.stringify(err));
+
+        return;
+      }
+
+      const { file } = files;
+      // console.log(files);
+      // console.log(file.originalPath);
+      // console.log(file);
+
+      const { compression } = fields;
+
+      if (!file || !compression) {
+        res.statusCode = 400;
+        res.end('Please select a file and compression type');
+
+        return;
+      }
+
+      const readStream = fs.createReadStream(file.filepath);
+      console.log(readStream);
+
+      readStream.on('error', () => {});
+
+      // readStream.pipe(fs.createWriteStream(`uploads/${file.filepath}`));
+
+      let extension;
+      let compressionStream;
+
+      switch (compression) {
+        case 'gzip':
+          extension = '.gzip';
+          compressionStream = zlib.createGzip();
+          break;
+        case 'deflate':
+          extension = '.dfl';
+          compressionStream = zlib.createDeflate();
+          break;
+        case 'br':
+          extension = '.br';
+          compressionStream = zlib.createBrotliCompress();
+          break;
+        default:
+          res.statusCode = 400;
+          res.end('Please select a valid compression type');
+
+          return;
+      }
+
+      const fileName = file.originalFilename + extension;
+
+      pipeline(readStream, compressionStream, res, (error) => {
+        if (error) {
+          // eslint-disable-next-line no-console
+          console.log(error);
+
+          res.end(JSON.stringify(err));
+        }
+      });
+      res.setHeader('Content-Type', 'application/octet-stream');
+
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${fileName}`
+      );
+      res.on('close', () => readStream.destroy());
+
+      res.statusCode = 200;
+      res.end();
+    });
+  } else {
+    res.on('error', () => 'res  error');
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+
+    res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+        <form action="/upload" method="post" enctype="multipart/form-data">
+          <label for="file">
+            Select file:
+            <input id="file" type="file" name="file"/>
+          </label>
+
+          <label for="type">
+            Select type of compression:
+            <select name="compression">
+              <option value="gzip">Gzip</option>
+              <option value="deflate">Deflate</option>
+              <option value="br">Brotli</option>
+            </select>
+          </label>
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+      </body>
+    </html>
+  `);
+  }
+});
+
+server.on('error', () => {});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
