e67aa915e018fbb379e34f6f9ba82236dd691933
Hishchak Tetiana <tanaiosered@ukr.net>
2024-01-19 21:14:51 +0200

    delete comment.

--- src/index.js
+++ src/index.js
@@ -5,45 +5,33 @@
 import http from 'node:http';
 import formidable, {errors as formidableErrors} from 'formidable';
 import fs from 'fs';
-import path from 'node:path';
 import zlib from 'zlib';
 
 const server = http.createServer(async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
-  // const fileName = url.pathname.slice(1) || 'index.html';
-  // const filePath = path.resolve('public', fileName);
 
   if (req.url === '/download' && req.method.toLowerCase() === 'post') {
-    // parse a file upload
     const form = formidable({});
-    let fields;
+    let formFields;
     let files;
     try {
-      [fields, files] = await form.parse(req);
+      [formFields, files] = await form.parse(req);
     } catch (err) {
-      // example to check for a very specific error
-      if (err.code === formidableErrors.maxFieldsExceeded) {
-
-      }
       console.error(err);
       res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
       res.end(String(err));
       return;
     }
-    // res.writeHead(200, { 'Content-Type': 'application/json' });
-    const type = fields.archiveType[0];
+
+    const type = formFields.archiveType[0];
 
     res.setHeader('Content-Encoding', type);
-    console.log(fields.archiveType[0]);
-
-    const fileStream = fs.createReadStream(fields.file[0]);
-    console.log (fields.file[0]);
-
+    const fileStream = fs.createReadStream(formFields.file[0]);
     const file = type === 'br' ? zlib.createBrotliCompress()  : zlib.createGzip();
 
 
     fileStream.pipe(file).pipe(res);
-    const fileName = fields.file[0] + '.' + type;
+    const fileName = formFields.file[0] + '.' + type;
     res.setHeader('Content-Type', 'application/octet-stream');
 
     res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
@@ -51,7 +39,6 @@ const server = http.createServer(async (req, res) => {
     res.statusCode = 200;
   }
 
-  // show a file upload form
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(`
     <h1>Create file</h1>
