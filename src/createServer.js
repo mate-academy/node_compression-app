44398a2661141047a54f879e07ef985c37a7765f
Anastasiya145 <a.ivanova-external@powernext.com>
2024-01-25 10:45:02 +0100

    small fix

--- src/createServer.js
+++ src/createServer.js
@@ -3,63 +3,9 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-const zlib = require('zlib');
 const formidable = require('formidable');
 const { pipeline } = require('stream');
-
-function getDataFromForm(req, res) {
-  const form = new formidable.IncomingForm();
-
-  return new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (!fields.compression || !files.file) {
-        res.statusCode = 400;
-        res.end('Error: You need to choose a file and a compression type');
-        reject(err);
-      } else {
-        const compressionType = fields.compression[0];
-        const file = files.file[0];
-
-        resolve({
-          compressionType, file,
-        });
-      }
-    });
-  });
-}
-
-function getParamsForCompressedFile(res, compressionType, originalFileName) {
-  let extention;
-  let compressionStream;
-
-  switch (compressionType) {
-    case 'gzip':
-      extention = '.gz';
-      compressionStream = zlib.createGzip();
-      break;
-
-    case 'deflate':
-      extention = '.dfl';
-      compressionStream = zlib.createDeflate();
-      break;
-
-    case 'br':
-      extention = '.br';
-      compressionStream = zlib.createBrotliCompress();
-      break;
-
-    default:
-      res.statusCode = 400;
-      res.end('This compression type is unsupported!');
-      break;
-  }
-
-  const newFileName = originalFileName + extention;
-
-  return {
-    newFileName, compressionStream,
-  };
-}
+const { getCompressingParams } = require('./modules/getCompressingParams');
 
 function createServer() {
   const server = new http.Server();
@@ -68,44 +14,57 @@ function createServer() {
     const url = new URL(req.url, `http://${req.headers.host}`);
 
     if (url.pathname === '/') {
-      const formPagePath = path.join(__dirname, 'index.html');
+      const fileName = url.pathname.slice(1) || 'index.html';
+      const formPagePath = path.resolve('public', fileName);
       const formStream = fs.createReadStream(formPagePath);
 
       res.writeHead(200, { 'Content-type': 'text/html' });
       formStream.pipe(res);
     } else if (url.pathname === '/compress' && req.method === 'POST') {
-      getDataFromForm(req, res)
-        .then(data => {
-          const { compressionType, file } = data;
+      const form = new formidable.IncomingForm();
 
-          const { newFileName, compressionStream } = getParamsForCompressedFile(
-            res,
-            compressionType,
-            file.originalFilename,
-          );
-
-          // console.log(file.filepath);
-          const readFileStream = fs.createReadStream(file.filepath);
-
-          pipeline(readFileStream, compressionStream, res, (err) => {
-            res.statusCode = 500;
-            res.end(JSON.stringify(err));
-          });
-
-          res.statusCode = 200;
-
-          res.setHeader(
-            'content-disposition',
-            `attachment; filename=${newFileName}`
-          );
-          res.end();
-
-          res.on('close', () => readFileStream.destroy());
-        })
-        .catch(err => {
+      form.parse(req, (err, fields, files) => {
+        if (!fields.compression || !files.file) {
           res.statusCode = 400;
-          res.end(`'Error processing the form data': ${err}`);
+          res.end('Please provide a compresion type and import file');
+
+          return;
+        }
+
+        if (err) {
+          res.statusCode = 400;
+          res.end(`Error found: ${err}`);
+
+          return;
+        }
+
+        const compressionType = fields.compression[0];
+        const file = files.file[0];
+
+        const {
+          extention,
+          compressionStream,
+        } = getCompressingParams(compressionType, res);
+
+        const newFileName = file.originalFilename + extention;
+
+        const readFileStream = fs.createReadStream(file.filepath);
+
+        pipeline(readFileStream, compressionStream, res, (error) => {
+          res.statusCode = 500;
+          res.end(JSON.stringify(error));
         });
+
+        res.statusCode = 200;
+
+        res.setHeader(
+          'content-disposition',
+          `attachment; filename=${newFileName}`
+        );
+        res.end();
+
+        res.on('close', () => readFileStream.destroy());
+      });
     } else if (url.pathname === '/compress' && req.method === 'GET') {
       res.statusCode = 400;
       res.end('Can not open this page. Before go to home page');
