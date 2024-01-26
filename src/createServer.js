14bcc14900ad414ae6f21cae3e9ac0cfd01d3e77
Kateryna Sydorenko <katerynnnna@gmail.com>
2024-01-26 21:55:56 +0200

    task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,62 @@
 'use strict';
 
+const fs = require('node:fs');
+const multer = require('multer');
+const path = require('node:path');
+const zlib = require('node:zlib');
+const express = require('express');
+const { pipeline } = require('node:stream');
+const bodyParser = require('body-parser');
+
+const map = {
+  gzip: zlib.createGzip(),
+  deflate: zlib.createDeflate(),
+  br: zlib.createBrotliCompress(),
+};
+
+const extentionMap = {
+  gzip: 'gz',
+  deflate: 'dfl',
+  br: 'br',
+};
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const app = express();
+  const upload = multer({ dest: '../public/' });
+
+  app.use(bodyParser.urlencoded());
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  });
+
+  app.post('/compress', upload.single('uploaded'), (req, res) => {
+    const { compressionType } = req.body;
+    const { file } = req;
+
+    if (!file || !compressionType || !map[compressionType]) {
+      res.status(400);
+      res.send();
+    }
+
+    const compressFunction = map[compressionType];
+    const extention = extentionMap[compressionType];
+    const compressedPath = `../public/${file.originalname}.${extention}`;
+
+    const source = fs.createReadStream(file.path);
+    const destination = fs.createWriteStream(compressedPath);
+
+    pipeline(source, compressFunction, destination, (err) => {
+      if (err) {
+        res.status(500);
+        res.send();
+      }
+
+      res.download(compressedPath);
+    });
+  });
+
+  return app;
 }
 
 module.exports = {
