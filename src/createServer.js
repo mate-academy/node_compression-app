549a8e0566c0e94fb0935a1fa3392c7cb005382e
boytrend0108 <boytrend@gmail.com>
2024-03-20 12:00:46 +0200

    create server

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,68 @@
+/* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { getContentType } = require('../helpers/getContentType');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = new http.Server();
+
+  server.on('request', (req, res) => {
+    const url = new URL(req.url, `http:${req.headers.host}`);
+    const filename = url.pathname.slice(1) || 'index.html';
+    const pathToFile = path.resolve('public', filename);
+
+    if (req.url === '/compress') {
+      const chunks = [];
+
+      req.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      req.on('end', () => {
+        const file = Buffer.concat(chunks);
+        const contentType = getContentType(file.toString());
+        const name = file.toString().split('=').pop();
+
+        res.writeHead(200, { 'Content-Type': contentType });
+
+        const filePath = path.join('public', name, 'test.txt');
+
+        const writeStream = fs.createWriteStream(filePath);
+
+        writeStream.write(file);
+        writeStream.end();
+      });
+
+      return;
+    }
+
+    if (!fs.existsSync(pathToFile)) {
+      res.statusCode = 404;
+      res.end('Not found');
+
+      return;
+    }
+
+    const fileStream = fs.createReadStream(pathToFile);
+
+    fileStream.on('error', () => {
+      res.statusCode = 500;
+      console.log('Stream error');
+      res.end('');
+    });
+
+    fileStream.pipe(res);
+    fileStream.on('close', () => fileStream.destroy());
+  });
+
+  server.on('error', () => {
+    console.log('Server error: Server instance');
+  });
+
+  return server;
 }
 
 module.exports = {
