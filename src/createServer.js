122c5c5f9b9cfeb32bde79a7a60cc35ce10751f4
boytrend0108 <boytrend@gmail.com>
2024-03-20 19:47:13 +0200

    downloaf false

--- src/createServer.js
+++ src/createServer.js
@@ -4,36 +4,62 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { formidable } = require('formidable');
+
 const { getContentType } = require('../helpers/getContentType');
 
 function createServer() {
   const server = new http.Server();
 
-  server.on('request', (req, res) => {
+  server.on('request', async (req, res) => {
     const url = new URL(req.url, `http:${req.headers.host}`);
     const filename = url.pathname.slice(1) || 'index.html';
     const pathToFile = path.resolve('public', filename);
 
-    if (req.url === '/compress') {
-      const chunks = [];
+    if (req.method.toLowerCase() === 'post' && req.url === '/compress') {
+      const form = formidable({});
+      let files;
+      // let fields;
 
-      req.on('data', (chunk) => {
-        chunks.push(chunk);
+      try {
+        [, files] = await form.parse(req);
+      } catch (err) {
+        console.error(err);
+        res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+        res.end(String(err));
+
+        return;
+      }
+
+      if (!files || !files.multipleFiles || !files.multipleFiles.length) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('No files uploaded');
+
+        return;
+      }
+
+      const file = files.multipleFiles[0];
+      const readstream = fs.createReadStream(file.filepath);
+
+      res.setHeader('Content-Type', getContentType(file.originalFilename));
+
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${file.originalFilename}`,
+      );
+
+      readstream.on('error', (err) => {
+        console.error('File not found:', err);
       });
 
-      req.on('end', () => {
-        const file = Buffer.concat(chunks);
-        const contentType = getContentType(file.toString());
-        const name = file.toString().split('=').pop();
+      readstream.pipe(res);
 
-        res.writeHead(200, { 'Content-Type': contentType });
-
-        const filePath = path.join('public', name, 'test.txt');
-
-        const writeStream = fs.createWriteStream(filePath);
-
-        writeStream.write(file);
-        writeStream.end();
+      fs.unlink(file.filepath, (err) => {
+        if (err) {
+          console.error(err);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+        }
       });
 
       return;
