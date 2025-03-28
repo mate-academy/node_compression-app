9911efb36656618ae84652663dc56a70afe507b2
Korshun <dmitriy.kor91@gmail.com>
2025-03-28 14:03:55 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,118 @@
 'use strict';
 
+const http = require('http');
+const zlib = require('zlib');
+const multiparty = require('multiparty');
+const fs = require('fs');
+const path = require('path');
+
+const html = `
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File compression</title>
+</head>
+
+<body>
+    <h1>Стиснення файлів</h1>
+
+    <form action='./compress' id='uploadForm' enctype="multipart/form-data" style="display: flex; flex-direction: column; max-width: 400px;">
+      <label for='file'>Оберіть файл:</label>
+      <input type='file' id="file" name='file' required>
+
+      <label for='compressionType'>Оберіть тип стиснення:</label>
+      <select id="compressionType" name='compressionType' style="max-width: 200px;">
+        <option value='gzip'>Gzip</option>
+        <option value='deflate'>Deflate</option>
+        <option value='br'>Br</option>
+      </select>
+
+      <button type='submit' style="max-width: 200px;">Стиснути файл</button>
+    </form>
+</body>
+</html>
+`;
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end(html);
+    } else if (req.url === '/compress' && req.method === 'POST') {
+      const form = new multiparty.Form();
+
+      form.parse(req, (parseError, fields, files) => {
+        if (parseError) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('File upload error.');
+
+          return;
+        }
+
+        const file = files.file[0];
+        const compressionType = fields.compressionType[0];
+
+        if (!['gzip', 'deflate', 'br'].includes(compressionType)) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Unsupported compression type.');
+
+          return;
+        }
+
+        if (!file || file.length === 0) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('No file uploaded.');
+
+          return;
+        }
+
+        fs.readFile(file.path, (err, data) => {
+          if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('File reading error.');
+
+            return;
+          }
+
+          let compressedData;
+          const fileName = path.basename(
+            file.originalFilename,
+            path.extname(file.originalFilename),
+          );
+          const fileExtension =
+            compressionType === 'gzip'
+              ? '.gz'
+              : compressionType === 'deflate'
+                ? '.dfl'
+                : '.br';
+          const outputFileName = `${fileName}${fileExtension}`;
+
+          if (compressionType === 'gzip') {
+            compressedData = zlib.gzipSync(data);
+          } else if (compressionType === 'deflate') {
+            compressedData = zlib.deflateSync(data);
+          } else if (compressionType === 'br') {
+            compressedData = zlib.brotliCompressSync(data);
+          }
+
+          res.writeHead(200, {
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': `attachment; filename='${outputFileName}'`,
+          });
+
+          res.end(compressedData);
+        });
+      });
+    } else if (req.url === '/compress' && req.method === 'GET') {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('POST request expected.');
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  });
 }
 
 module.exports = {
